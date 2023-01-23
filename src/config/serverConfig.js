0f23e238c343b3934a22e29184ce473329e12f34
4souravsinha <4souravsinhain@gmail.com>
2023-01-23 23:17:28 +0530

    added business logic to booking service

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 module.exports = {
     PORT: process.env.PORT,
-    DB_SYNC: process.env.DB_SYNC
+    DB_SYNC: process.env.DB_SYNC,
+    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH
 }
